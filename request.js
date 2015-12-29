0dd991a35716b1c83a92e25a05f79246ebc8040b
Ruth Johnson <ruthjohnson2013@gmail.com>
2015-12-28 19:16:14 -0800

    Fixed loading errors

--- request.js
+++ request.js
@@ -1,57 +1,32 @@
- (function(d){
-     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement('script'); js.id = id; js.async = true;
-     js.src = "http://connect.facebook.net/en_US/all.js";
-     ref.parentNode.insertBefore(js, ref);
- }(document));
 
- function getNotif()
-{
-	FB.api('me/?fields=notifications', function(response) 
-	{
-		for(var i = 0; i<10;i++) 
-		{
-					
-				console.log(response.notifications.data[i]);
-				traceNotif(response.notifications.data[i].id);
-			
-		}
-    	});
-}
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '1669165146686447',
+          xfbml      : true,
+          version    : 'v2.1'
+        });
+      };
+
+        (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "https://connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+  
 
 
-function traceEvent(id,domain)
-{
-	FB.api('/'+id+'', function(response)
-	{	var str = "";
-		var indicatorMust = {value: false};
-		var indicatorOpt = {value: false};
-		for (var j=0; j<3; j++)
+var id = 4983859492027 
+
+function getEvents(){
+	FB.api('/'+id+'/events', function(response){
+		for (var i=0; i<10; i++)
 		{
-			parse(indicatorMust, response.title, wordMusthave[j]);
+			console.log(response.data[i].name)
+			getMemberEvents(response.data[i].id);
 		}
-		parse(indicatorOpt, response.title, wordOpt.value);
-		if(indicatorMust.value == true && indicatorOpt.value== true)
-		{
-	  	/////////////////////
-	  //	console.log("printed");
-	  //	getCover(id,img);
-	  	str= "<div class='row'><div class='col-sm-6 col-md-10'><div class='thumbnail'>"
-	  	+"<p id='vivian'></p><div class='caption'>"
-	  	+"<h2><font color='black'>" +response.name+"</font></h2>"
-	  	+"<p>"+response.description+"</p>"
-	  	+"<p><a href='https://www.facebook.com/events/"+response.id+"/' class='btn btn-primary' role='button'>"
-	  	+"Event Page</a> </p></div></div></div></div>";
-		}
-		
-	//	console.log(response.name);
-		if(domain=="forya")
-		{document.getElementById("forya").innerHTML+=str;}
-		else if(domain=="forworld")
-		{console.log("PLEASE WORK!");
-		document.getElementById("forworld").innerHTML+=str;}
-		
-		
 	});
 }
+
+
